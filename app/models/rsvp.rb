db3ed61c81e346f4851d87d9a80e229e0d8d0c13
jazzqueenjessie <jazzqueenjessie@gmail.com>
2013-08-26 09:45:52 -0700

    add validations to input fields

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,8 +1,18 @@
 class Rsvp
   extend ActiveModel::Naming
   include ActiveModel::Conversion
+  include ActiveModel::Validations
   
   attr_accessor :name, :email, :message
+  
+  validates :name,
+            :presence => true
+  
+  validates :email,
+            :format => { :with => /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ }
+  
+  validates :message,
+            :length => { :minimum => 10, :maximum => 1000 }
   
   def initialize(attributes = {})
     attributes.each do |name, value|
@@ -11,6 +21,7 @@ class Rsvp
   end
   
   def deliver
+    return false unless valid?
     true
   end
   
